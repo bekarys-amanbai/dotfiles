7d743dbcf27d861f7f522850d5024babfae5a058
bekarys-amanbai <57945589+bekarys-amanbai@users.noreply.github.com>
2021-09-02 19:56:08 +0600

    init emacs config

---
+++ init.el
@@ -0,0 +1,60 @@
+;; БАЗОВЫЕ НАСТРОЙКИ
+;; Пишем название открытого буфера в шапке окна
+(setq frame-title-format "GNU Emacs: %b")
+
+(setq inhibit-splash-screen   t)
+(setq ingibit-startup-message t) ;; экран приветствия можно вызвать комбинацией C-h C-a
+
+(show-paren-mode t) ;; включить выделение выражений между {},[],()
+(setq show-paren-style 'expression) ;; выделить цветом выражения между {},[],()
+
+(tooltip-mode      -1)
+(blink-cursor-mode -1) ;; курсор не мигает
+(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
+
+;(menu-bar-mode     -1) ;; отключаем графическое меню
+(tool-bar-mode     -1) ;; отключаем tool-bar
+(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
+(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
+(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
+
+(setq make-backup-files        nil)
+(setq auto-save-default        nil)
+(setq auto-save-list-file-name nil)
+
+;; нумирация строк
+(require 'linum)
+(line-number-mode   t) ;; показать номер строки в mode-line
+(global-linum-mode  t) ;; показывать номера строк во всех буферах
+(column-number-mode t) ;; показать номер столбца в mode-line
+(setq linum-format " %d") ;; задаем формат нумерации строк
+
+
+(require 'ido)
+(ido-mode t)
+
+
+
+;; НАСТРОЙКИ ПЛАГИНОВ
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+;; (package-refresh-contents)
+
+
+
+
+;; THEMES
+(unless (package-installed-p 'spacemacs-theme)
+  (package-install 'spacemacs-theme))
+(load-theme 'spacemacs-dark t)
+
+
+;; Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+(require 'evil)
+(evil-mode 1)
+
