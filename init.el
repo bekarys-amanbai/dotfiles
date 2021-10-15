d4ffbf5f37380b4d40dd7a51932abb79dc4de441
bekarys-amanbai <57945589+bekarys-amanbai@users.noreply.github.com>
2021-10-16 01:51:29 +0600

    add use-package and reorganizing packages for it
    +elpy - python ide

--- init.el
+++ init.el
@@ -1,32 +1,31 @@
 ;; БАЗОВЫЕ НАСТРОЙКИ
-;; Пишем название открытого буфера в шапке окна
-(setq frame-title-format "GNU Emacs: %b")
 
-(setq inhibit-splash-screen   t)
-(setq ingibit-startup-message t) ;; экран приветствия можно вызвать комбинацией C-h C-a
+(setq frame-title-format "Emacs: %b")
 
-(show-paren-mode t) ;; включить выделение выражений между {},[],()
+;(menu-bar-mode                  -1) ;; отключаем графическое меню
+(tool-bar-mode                  -1) ;; отключаем tool-bar
+(scroll-bar-mode                -1) ;; отключаем полосу прокрутки
+(setq inhibit-splash-screen      t)
+(setq ingibit-startup-message    t) ;; отключаем экран приветствие
+(setq use-dialog-box           nil) ;; никаких графических диалогов и окон - все через минибуфер
+(setq redisplay-dont-pause       t)  ;; лучшая отрисовка буфера
+
+(tooltip-mode                   -1)
+(blink-cursor-mode              -1) ;; курсор не мигает
+(setq ring-bell-function   'ignore) ;; отключить звуковой сигнал
+(show-paren-mode                 t) ;; включить выделение выражений между {},[],()
 (setq show-paren-style 'expression) ;; выделить цветом выражения между {},[],()
-
-(tooltip-mode      -1)
-(blink-cursor-mode -1) ;; курсор не мигает
-(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
-
-;(menu-bar-mode     -1) ;; отключаем графическое меню
-(tool-bar-mode     -1) ;; отключаем tool-bar
-(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
-(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
-(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
 
 (setq make-backup-files        nil)
 (setq auto-save-default        nil)
 (setq auto-save-list-file-name nil)
 
+
 ;; нумирация строк
 (require 'linum)
-(line-number-mode   t) ;; показать номер строки в mode-line
-(global-linum-mode  t) ;; показывать номера строк во всех буферах
-(column-number-mode t) ;; показать номер столбца в mode-line
+(line-number-mode      t) ;; показать номер строки в mode-line
+(global-linum-mode     t) ;; показывать номера строк во всех буферах
+(column-number-mode    t) ;; показать номер столбца в mode-line
 (setq linum-format " %d") ;; задаем формат нумерации строк
 
 
@@ -45,18 +44,27 @@
   (package-refresh-contents))
 
 
+;; USE-PACKAGE
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 
 ;; THEMES
-(unless (package-installed-p 'spacemacs-theme)
-  (package-install 'spacemacs-theme))
-(load-theme 'spacemacs-dark t)
+(use-package spacemacs-theme
+  :ensure t
+  :config
+  (load-theme 'spacemacs-dark t))
 
 
 ;; Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
 
-(require 'evil)
-(evil-mode 1)
 
+;; Elpy
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
